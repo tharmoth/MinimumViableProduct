db7e25e9ea002fce0c4520b2558822fa1b1fce2e
AnonymousFood <56987492+AnonymousFood@users.noreply.github.com>
2023-05-28 18:25:57 -0500

    Moved most scenes to folders

--- Scenes/Enemy/Enemy.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody2D
-
-const SPEED = 300.0
-
-func _physics_process(delta):
-	var direction = Input.get_axis("ui.left", "ui.right")
-	velocity.x = move_toward(velocity.x, 200, SPEED)
-	move_and_slide()
