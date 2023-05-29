84ade06ba1e4a77294f77877a47689afe9249475
AnonymousFood <56987492+AnonymousFood@users.noreply.github.com>
2023-05-28 19:45:45 -0500

    Added Turret targeting logic

--- Scenes/Turret/Turret.gd
+++ Scenes/Turret/Turret.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+var targets: Array[Node2D]
+
+@export var rot_speed: float = 5.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +11,26 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	# update draw
+	queue_redraw()
+	if not targets.is_empty():
+		# handle the gun rotation
+		var target_pos: Vector2 = targets.front().global_position
+		var target_rot: float = global_position.direction_to(target_pos).angle()
+		rotation = lerp_angle(rotation, target_rot, rot_speed * delta)
+
+func _on_body_entered(body):
+	if not body in targets:
+		targets.append(body)
+
+func _on_body_exited(body):
+	if body in targets:
+		targets.erase(body)
+
+func _on_area_entered(area):
+	if not area in targets:
+		targets.append(area)
+
+func _on_area_exited(area):
+	if area in targets:
+		targets.erase(area)
