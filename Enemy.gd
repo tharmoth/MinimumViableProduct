644c52ebe9d6243d5eeb5988bf4a41c2f8ffadcb
Lizzy <55554146+lizzyLoki@users.noreply.github.com>
2023-05-28 22:06:50 -0500

    Update the movement of the Enemy
    Added
    1. invisible sprites
    2. speed logic
    3. movement of grandma between sprites
    4. NavigationPoints scene

--- Enemy.gd
+++ Enemy.gd
@@ -1,34 +1,54 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var percentToTarget = 0.0
+var currentPointIdx = 0
+var pxlsAlreadyTraveled = 0
+var speed = 500.0
+var distance = 0
+var targetPoint = 0
 
 func _physics_process(delta):
-	#move_and_slide()
-	var direction = Input.get_axis("ui.left", "ui.right")
-	velocity.x = move_toward(velocity.x, 200, SPEED)
-	move_and_slide()
+	var invis = get_node("/root/Main/NavigationPoints")
+	var currentPoint = invis.get_child(currentPointIdx)
+	if (currentPointIdx + 1) < invis.get_child_count():
+		targetPoint = invis.get_child(currentPointIdx + 1)
+		distance = targetPoint.position.distance_to(currentPoint.position)
+	
+	if percentToTarget <= 1:
+		pxlsAlreadyTraveled += delta * speed
+		percentToTarget = pxlsAlreadyTraveled/distance
+		$EnemySprite.position = currentPoint.position.lerp(targetPoint.position, percentToTarget)
+	else:
+		currentPointIdx = currentPointIdx + 1
+		pxlsAlreadyTraveled = 0
+		percentToTarget = 0.0
 
-#
-#func _physics_process(delta):
-#	# Add the gravity.
-#	if not is_on_floor():
-#		velocity.y += gravity * delta
-#
-#	# Handle Jump.
-#	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-#		velocity.y = JUMP_VELOCITY
-#
-#	# Get the input direction and handle the movement/deceleration.
-#	# As good practice, you should replace UI actions with custom gameplay actions.
-#	var direction = Input.get_axis("ui_left", "ui_right")
-#	if direction:
-#		velocity.x = direction * SPEED
-#	else:
-#		velocity.x = move_toward(velocity.x, 0, SPEED)
-#
-#	move_and_slide()
+	
+#	var invis1 = invis.get_child(0)
+#	var invis2 = invis.get_child(1)
+#	var invis3 = invis.get_child(2)
+#	var invis4 = invis.get_child(3)
+#	var invis5 = invis.get_child(4)
+#	var invis6 = invis.get_child(5)
+
+	
+	
+	
+	
+	
+#	if $EnemySprite.position == invis2.position:
+#		if target2 <= 1:
+#			target2 += delta * 0.9
+#		$EnemySprite.position = invis2.position.lerp(invis3.position, target2)
+#	if $EnemySprite.position == invis3.position:
+#		if target3 <= 1:
+#			target3 += delta * 0.4
+#		$EnemySprite.position = invis3.position.lerp(invis4.position, target3)
+#	if $EnemySprite.position == invis4.position:
+#		if target4 <= 1:
+#			target4 += delta * 0.9
+#		$EnemySprite.position = invis4.position.lerp(invis5.position, target4)
+#	if $EnemySprite.position == invis5.position:
+#		if target5 <= 1:
+#			target5 += delta * 0.4
+#		$EnemySprite.position = invis5.position.lerp(invis6.position, target5)
