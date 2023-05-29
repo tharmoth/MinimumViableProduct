adea014135d6f069bf8e92cc04cd3b6ce9be988b
AnonymousFood <56987492+AnonymousFood@users.noreply.github.com>
2023-05-28 21:57:05 -0500

    Improved turret things more

---
+++ Scenes/Bullet/bullet.gd
@@ -0,0 +1,42 @@
+extends Area2D
+
+@export var lifetime: int = 10  # seconds
+
+var speed: int
+var damage: int
+var velocity: Vector2
+
+@onready var lifetime_timer := $LifetimeTimer as Timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# create a one-shot timer for lifetime
+	lifetime_timer.start(lifetime)
+
+func _physics_process(delta: float) -> void:
+	global_position += velocity * delta
+
+func start(_position: Vector2, _rotation: float, _speed: int, _damage: int, _target=null) -> void:
+	global_position = _position
+	rotation = _rotation
+	speed = _speed
+	damage = _damage
+	velocity = Vector2.RIGHT.rotated(_rotation) * speed
+
+func _on_body_entered(body):
+	# TODO body takes damage
+	_explode()
+	
+func _on_area_entered(area):
+	# TODO area takes damage?
+	_explode()
+
+func _explode() -> void:
+	# stop the bullet and disable collision
+	set_physics_process(false)
+	$CollisionShape2D.set_deferred("disabled", true)
+	$Sprite2D.hide()
+	
+func _on_lifetime_timer_timeout() -> void:
+	queue_free()
+
--- Scenes/Turret/Turret.gd
+++ Scenes/Turret/Turret.gd
@@ -1,13 +1,36 @@
 extends Area2D
 
 var targets: Array[Node2D]
+var can_shoot := true
 
-@export var rot_speed: float = 5.0
+@export var fire_rate: float = 0.1
+@export var rot_speed: float = 8.0
+@export var projectile_type: PackedScene
+@export var projectile_spread: float = 0.2
+@export var projectile_speed: int = 1000
+@export var projectile_damage: int = 10
+
+@onready var firerate_timer := $FireRateTimer as Timer
+@onready var muzzle := $TurretSprite2D/Muzzle as Marker2D
+@onready var gun := $TurretSprite2D as Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+# Gets called by its parents, so that we have more control over when to shoot
+func shoot() -> void:
+	can_shoot = false
+	_instance_projectile(muzzle.global_position)
+	# show reload time on HUD
+	firerate_timer.start(fire_rate)
+
+func _instance_projectile(_position: Vector2, target=null) -> void:
+	var bullet = preload("res://Scenes/Bullet/bullet.tscn").instantiate()
+	add_child(bullet)
+	bullet.start(_position,
+			gun.rotation + randf_range(-projectile_spread, projectile_spread),
+			projectile_speed, projectile_damage, target)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,7 +40,11 @@ func _process(delta):
 		# handle the gun rotation
 		var target_pos: Vector2 = targets.front().global_position
 		var target_rot: float = global_position.direction_to(target_pos).angle()
-		rotation = lerp_angle(rotation, target_rot, rot_speed * delta)
+		gun.rotation = lerp_angle(gun.rotation, target_rot, rot_speed * delta)
+		if can_shoot and abs(gun.rotation - target_rot) < 0.3:
+			print("Gun Rotation:", gun.rotation)
+			print("Target Rotation:", target_rot)
+			shoot()
 
 func _on_body_entered(body):
 	if not body in targets:
@@ -34,3 +61,6 @@ func _on_area_entered(area):
 func _on_area_exited(area):
 	if area in targets:
 		targets.erase(area)
+
+func _on_fire_rate_timer_timeout():
+	can_shoot = true
